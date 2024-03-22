0c51edb3c9ccdb582f58186b0a4a8bf851d523de
Abuzar RaziQ <abuzarraziqo@gmail.com>
2024-03-22 16:51:37 +0500

    project-3 completed

---
+++ Project-3-RGB-color Picker/logic.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// DOM Elements:
+const redSlider = document.getElementById("redSlider");
+const greenSlider = document.getElementById("greenSlider");
+const blueSlider = document.getElementById("blueSlider");
+
+const redValueSpan = document.getElementById("redValue");
+const greenValueSpan = document.getElementById("greenValue");
+const blueValueSpan = document.getElementById("blueValue");
+
+const colorBox = document.getElementById("color-box");
+const copyButton = document.getElementById("copyButton");
+const inputTypeRgbValue = document.getElementById("inputType");
+
+// RGB Value Update:
+const updateColor = function () {
+  const redValue = redSlider.value;
+  const greenValue = greenSlider.value;
+  const blueValue = blueSlider.value;
+
+  const rgbColor = `rgb(${redValue},${greenValue},${blueValue})`;
+  colorBox.style.backgroundColor = rgbColor;
+
+  redValueSpan.textContent = redValue;
+  greenValueSpan.textContent = greenValue;
+  blueValueSpan.textContent = blueValue;
+  inputTypeRgbValue.textContent = rgbColor;
+};
+updateColor();
+
+// Copy RGB Value:
+const copyRGBValue = function () {
+  const redValue = redSlider.value;
+  const greenValue = greenSlider.value;
+  const blueValue = blueSlider.value;
+
+  const rgbColor = `rgb(${redValue},${greenValue},${blueValue})`;
+
+  navigator.clipboard
+    .writeText(rgbColor)
+    .then(() => {
+      alert("RGB Color Value Copied to clipboard" + rgbColor);
+    })
+    .catch((error) => {
+      console.error("Failed to copy RGB Value:", error);
+    });
+};
+
+// Event Listeners
+redSlider.addEventListener("input", updateColor);
+greenSlider.addEventListener("input", updateColor);
+blueSlider.addEventListener("input", updateColor);
+copyButton.addEventListener("click", copyRGBValue);
